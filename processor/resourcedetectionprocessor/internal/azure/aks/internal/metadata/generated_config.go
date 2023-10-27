dd0ca5e339ebfa2c62bf04519db59fc053cd621c
jsirianni <joe.sirianni@observiq.com>
2023-10-27 17:15:26 -0400

    wip: detect aks cluster name

--- processor/resourcedetectionprocessor/internal/azure/aks/internal/metadata/generated_config.go
+++ processor/resourcedetectionprocessor/internal/azure/aks/internal/metadata/generated_config.go
@@ -2,31 +2,16 @@
 
 package metadata
 
-import "go.opentelemetry.io/collector/confmap"
-
 // ResourceAttributeConfig provides common config for a particular resource attribute.
 type ResourceAttributeConfig struct {
 	Enabled bool `mapstructure:"enabled"`
-
-	enabledSetByUser bool
-}
-
-func (rac *ResourceAttributeConfig) Unmarshal(parser *confmap.Conf) error {
-	if parser == nil {
-		return nil
-	}
-	err := parser.Unmarshal(rac, confmap.WithErrorUnused())
-	if err != nil {
-		return err
-	}
-	rac.enabledSetByUser = parser.IsSet("enabled")
-	return nil
 }
 
 // ResourceAttributesConfig provides config for resourcedetectionprocessor/aks resource attributes.
 type ResourceAttributesConfig struct {
-	CloudPlatform ResourceAttributeConfig `mapstructure:"cloud.platform"`
-	CloudProvider ResourceAttributeConfig `mapstructure:"cloud.provider"`
+	CloudPlatform  ResourceAttributeConfig `mapstructure:"cloud.platform"`
+	CloudProvider  ResourceAttributeConfig `mapstructure:"cloud.provider"`
+	K8sClusterName ResourceAttributeConfig `mapstructure:"k8s.cluster.name"`
 }
 
 func DefaultResourceAttributesConfig() ResourceAttributesConfig {
@@ -35,6 +20,9 @@ func DefaultResourceAttributesConfig() ResourceAttributesConfig {
 			Enabled: true,
 		},
 		CloudProvider: ResourceAttributeConfig{
+			Enabled: true,
+		},
+		K8sClusterName: ResourceAttributeConfig{
 			Enabled: true,
 		},
 	}
